9b5ad8ab530e68fb4bdb8d42c7da3d17cc4b40f0
Isaac Alter <i_alter@yahoo.com>
2026-05-28 17:13:35 -0600

    refactor: updated the App file to be wrapped in a BrowserRouter to set up routes within it and for the DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,54 @@
 import { useEffect, useState } from "react";
 import { auth } from './firebase';
 import { onAuthStateChanged, type User } from "firebase/auth";
-import Login from './Login';
-import './App.css';
-import Dashboard from './Dashboard';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-function App() {
-  const [currentUser, setCurrentUser] = useState<User | null>(null);
+import Login from './Login';
+import Dashboard from "./Dashboard";
+
+export default function App() {
+  const [user, setUser] = useState<User | null>(null);
   const [loading, setLoading] = useState(true);
 
   //This sets up the listner. Whenever the user logs in or out, Firebase tells us.
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      setCurrentUser(user);
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
       setLoading(false);
     });
     return () => unsubscribe();
   }, []);
 
   if (loading) {
-    return <div style={{ textAlign: "center", marginTop: "100px" }}>Loading App...</div>;
+    return <div style={{ display: 'flex', justifyContent: 'center', alignItems: "center", height: '100vh' }}>Loading...</div>;
   }
 
-  //If they are NOT logged in, force them to the Login screen
-  if (!currentUser) {
+  //If they aren't loading, lock them on the Auth screen
+  if (!user) {
     return <Login />;
   }
 
-  //If they are logged in, show them the app
-  return <Dashboard user={currentUser} />;
-}
+  //If they are logged in, give access to the Router
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* The Home Page */}
+        <Route path="/" element={<Dashboard user={user} />} />
 
-export default App;
+        {/* The Dynamic Profile Page (:username acts as a variable) */}
+        <Route 
+          path="/user/:username"
+          element={
+            <div style={{ padding: '50px', textAlign: 'center' }}>
+              <h2>Profile Page Coming Soon...</h2>
+              <a href="/">Go back to Home</a>
+            </div>
+          }
+        />
+
+        {/* Catch-All: If a mistyped URL, redirect to Home */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
